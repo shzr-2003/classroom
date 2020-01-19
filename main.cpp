87c69ffb6671e9074f8c70202e64b68eea24ec89
shzr-2003 <48718838+shzr-2003@users.noreply.github.com>
2020-01-19 19:52:38 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 # include <bits/stdc++.h>
+# include <windows.h>
 # define R register int
 # define r(a,b) rand()%((b)-(a)+1)+(a)
 
@@ -7,8 +8,9 @@ using namespace std;
 const int Max_R=20;
 const int Max_C=30;
 int now_x,now_y,s_len,bie_cnt,times,beat_bie_cnt,limit_bie;
+int loli_cnt;
 char cr[100][100],st[100];
-string saying,bie[100];
+string saying,bie[100],loli[100];
 
 void get_map()
 {
@@ -42,6 +44,15 @@ void get_bieyu()
 	freopen("CON","r",stdin);
 }
 
+void get_loli()
+{
+	freopen("loli.in","r",stdin);
+	scanf("%d",&loli_cnt);
+    for (R i=1;i<=loli_cnt;++i)
+		cin>>loli[i];
+	freopen("CON","r",stdin);
+}
+
 void get_xy (int &x,int &y)
 {
 	x=r(2,Max_R-1); y=r(2,Max_C-1);
@@ -54,6 +65,7 @@ void make_new_bie()
 	if(beat_bie_cnt!=limit_bie)
 	{
 		cout<<"您还没有打完现有的这只鳖呢~"<<endl;
+		Sleep(2000);
 		return;
 	}
 	limit_bie=r(3,5);
@@ -61,6 +73,7 @@ void make_new_bie()
 	cr[x][y]='B';
 	beat_bie_cnt=0;
 	cout<<"您创造了一只新的鳖，开始快乐地打鳖吧！"<<endl; 
+	Sleep(2000);
 }
 
 void move()
@@ -72,13 +85,26 @@ void move()
 		int r=r(1,bie_cnt-1);
 		if(beat_bie_cnt==limit_bie) r=bie_cnt;
 		saying=bie[r];
-		cout<<saying<<endl;
-		printf("\n");
+		cout<<saying<<endl<<endl;
+		Sleep(2000);
 		if(beat_bie_cnt<limit_bie)
 		{
 			int x,y; get_xy(x,y);
 			cr[x][y]='B';
 			beat_bie_cnt++;
+		}
+	}
+	else
+	{
+		int p=r(1,5);
+		if(p==1)
+		{
+			get_loli();
+			int r=r(1,loli_cnt);
+			cout<<loli[r]<<endl;
+			cout<<"路遇loli，您只好回到座位上学习。"<<endl;
+			Sleep(2000);
+			now_x=now_y=10;
 		}
 	}
 }
@@ -92,7 +118,7 @@ int main()
     {
     	int x=0,y=0;
     	scanf("%s",st+1);
-		if(st[1]=='w'||st[1]=='a'||st[1]=='w'||st[1]=='d') 
+		if(st[1]=='w'||st[1]=='a'||st[1]=='s'||st[1]=='d') 
 			scanf("%d",&times);
     	if(st[1]=='w') x=now_x-times,y=now_y;
 		if(st[1]=='a') x=now_x,y=now_y-times;
